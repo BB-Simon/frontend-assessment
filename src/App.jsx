412265708dae229161b59b8dc319cd30b677a803
BB-Simon <simon.chowdery@gmail.com>
2025-01-07 20:04:42 +0200

    Setup webpack

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./app.css";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello, React with Webpack!</h1>
+      <div className="container">
+        <p>This is a simple React app using Webpack.</p>
+        <p>Created by me, Zara!</p>
+      </div>
+      <div className="bg-indigo-400 p-4 rounded-lg text-white">
+        <p>Hello test text</p>
+      </div>
+    </div>
+  );
+};
+
+export default App;
